399eb2f2a64ad4b908881ca4a1c5d50005ec6127
sugarhaining <1473734205@qq.com>
2019-01-31 15:42:18 +0800

    project

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="counter-warp">
+    <p>Vuex counter：{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+
+    <a href="/pages/index/main" class="home">去往首页</a>
+    <button type="primary" open-type="share" data-name="pageShare" id="share">点击分享</button>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  },
+  onShareAppMessage(res){
+
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
