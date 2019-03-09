0b808ed732e3e13fa4bc2315cc36e5b9aa881345
sweetamily <972041628@qq.com>
2019-03-09 19:35:17 +0800

    update

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -3,7 +3,7 @@
     <img :src="showCircle?bg_img[0]:bg_img[1]" :class="showCircle?'index-bg-img':'depart-bg-img'">
     <div class="content" v-if="showCircle">
       <span>志愿圈</span>
-      <img :src="shareIcon" @click="clickShare">
+      <img :src="shareIcon" @click="clickShare" class="img-share">
     </div>
     <div class="content" v-else>
       <div class="content_depart">
@@ -43,84 +43,83 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  @import '../style/common.scss';
-  .container{
+@import "../style/common.scss";
+.container {
+  width: $full_width;
+  height: cr(60);
+  position: relative;
+  overflow: hidden;
+  .index-bg-img {
+    width: cr(100);
+    height: cr(80);
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1000;
+  }
+  .depart-bg-img {
+    width: 100%;
+    height: 80%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+  }
+  .content {
     width: $full_width;
-    height: cr(60);
-    position: relative;
-    overflow: hidden;
-    .index-bg-img{
-      width: cr(100);
-      height: cr(80);
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: -1;
+    height: 100%;
+    padding-left: cr(28);
+    padding-right: cr(22);
+    box-sizing: border-box;
+    @include flex_row;
+    @include flex_between;
+    align-items: center;
+    font: {
+      weight: 500;
     }
-    .depart-bg-img{
-      width:100%;
-      height: 80%;
-      position: absolute;
-      top: 0;
-      left: 0; 
-      z-index: -1;
-    }
-    .content{
-      width: $full_width;
-      height: 100%;
-      padding-left: cr(28);
-      padding-right: cr(22);
-      box-sizing: border-box;
-      @include flex_row;
-      @include flex_between;
-      align-items: center;
-      font: {
-        weight: 500;
-      }
-      div{
-        @include flex_column;
-        align-items: flex-start;
-        .depart-title{
-          color: #F0BC76;
-          font: {
-            weight: 500;
-          }
-        }
-        .depart-name{
-          color: #D59FA5;
-          position: relative;
-          font: {
-            size: cr(12);
-          }
-          .depart-after{
-            width: 100%;
-            height: 40%;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            background-color: #F0BC76; 
-            z-index: -1;
-            opacity: .5;
-            border-radius: cr(5);
-          }
-        }
-      }
-      span{
-        color: #D67183;
+    div {
+      @include flex_column;
+      align-items: flex-start;
+      .depart-title {
+        color: #f0bc76;
         font: {
-          size:cr(20);
+          weight: 500;
+        }
+      }
+      .depart-name {
+        color: #d59fa5;
+        position: relative;
+        font: {
+          size: cr(12);
+        }
+        .depart-after {
+          width: 100%;
+          height: 40%;
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          background-color: #f0bc76;
+          z-index: -1;
+          opacity: 0.5;
+          border-radius: cr(5);
         }
       }
     }
     span {
+      color: #d67183;
       font: {
         size: cr(20);
       }
     }
-    img {
-      width: cr(22);
-      height: cr(22);
+  }
+  span {
+    font: {
+      size: cr(20);
     }
+  }
+  .img-share {
+    width: cr(22);
+    height: cr(22);
   }
 }
 </style>
